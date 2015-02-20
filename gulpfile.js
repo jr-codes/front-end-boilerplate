25325f81fc6e29d11de34a17635411f11cccc5e7
JR Shampang <jr.shampang@gmail.com>
2015-02-20 00:24:32 -0600

    Updated gulp plugins. Replaced connect, livereload, and opn with BrowserSync.

--- gulpfile.js
+++ gulpfile.js
@@ -1,109 +1,99 @@
 'use strict';
 
 var gulp = require('gulp');
-var	plugins = require('gulp-load-plugins')();
-var	env = plugins.util.env;
+var	$ = require('gulp-load-plugins')();
+var del = require('del');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var runSequence = require('run-sequence');
+
 var paths = {
-	html: './app/**/*.html',
-	sass: './app/styles/**/*.scss',
-	js: './app/scripts/**/*.js',
-	mainjs: './app/scripts/main.js',
-	build: './build/'
+    src: 'app',
+    dist: 'dist'
 };
 
 
-gulp.task('default', ['build', 'watch']);
+// Builds the project and starts a local server
+gulp.task('default', function(done) {
+    runSequence('build', 'serve', done);
+});
 
-gulp.task('build', ['build:html', 'build:css', 'build:js']);
 
+// Builds HTML, CSS, JS, and bumps project version
+gulp.task('build', ['build:html', 'build:css', 'build:js', 'bump'], function() {
+    return gulp.src(paths.dist + '/**')
+        .pipe($.size({ showFiles: true }));
+});
+
+
+// Copies HTML files
 gulp.task('build:html', function() {
-	return gulp.src(paths.html)
-		.pipe(gulp.dest(paths.build))
-		.pipe(plugins.size({ showFiles: true }));
+    return gulp.src(paths.src + '/**/*.html')
+        .pipe(gulp.dest(paths.dist));
 });
 
+
+// Compiles Sass files
 gulp.task('build:css', function() {
-	// --dev will skip minifying CSS
-	var sassOptions = env.dev ? {} : { outputStyle: 'compressed' };
-
-	return gulp.src(paths.sass)
-		.pipe(plugins.sass(sassOptions))
-		.pipe(plugins.autoprefixer('last 1 version'))
-		.pipe(gulp.dest(paths.build))
-		.pipe(plugins.size({ showFiles: true }));
+    return gulp.src(paths.src + '/**/*.scss')
+        .pipe($.sass({ outputStyle: 'compressed' }))
+        .pipe($.autoprefixer('last 1 version'))
+        .pipe($.flatten())
+        .pipe(gulp.dest(paths.dist))
+        .pipe(browserSync.reload({ stream: true }));
 });
 
+
+// Compiles CommonJS into minified JS
 gulp.task('build:js', ['jshint'], function() {
-    var bundler = require('browserify');
-    var source = require('vinyl-source-stream');
-    var buffer = require('vinyl-buffer');
-    var filename = paths.mainjs.split('/').pop();
-
-    // --dev will skip minifying JS
-    var uglify = function() {
-        return env.dev ? plugins.util.noop() : plugins.uglify({ preserveComments: 'some' });
-    };
-
-    return bundler(paths.mainjs)
+    return browserify('./' + paths.src + '/scripts/main.js')
         .bundle()
-        .pipe(source(filename))
+        .pipe(source('main.js'))
         .pipe(buffer())
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.build))
-        .pipe(plugins.size({ showFiles: true }));
+        .pipe($.uglify({ preserveComments: 'some' }))
+        .pipe(gulp.dest(paths.dist));
 });
 
 
+// Check JS for errors
 gulp.task('jshint', function() {
-	return gulp.src([paths.js, 'gulpfile.js'])
-		.pipe(plugins.jshint())
-		.pipe(plugins.jshint.reporter(require('jshint-stylish')));
-});
-
-gulp.task('connect', ['build'], function() {
-	var app = require('connect')()
-		.use(require('connect-livereload')({ port: 35729 }))
-		.use(require('serve-static')(paths.build))
-		.use(require('serve-index')(paths.build));
-
-	require('http').createServer(app)
-		.listen(9000)
-		.on('listening', function() {
-			console.log('Started server on http://localhost:9000');
-		});
-
-	require('opn')('http://localhost:9000');
-});
-
-gulp.task('watch', ['connect'], function() {
-	var server = plugins.livereload();
-
-    // Refresh browser when build files change
-	gulp.watch([
-		paths.build + '**/*.*'
-	]).on('change', function(file) {
-		server.changed(file.path);
-	});
-
-    // Rebuild files when source files change
-	gulp.watch(paths.html, ['build:html']);
-	gulp.watch(paths.sass, ['build:css']);
-	gulp.watch(paths.js, ['build:js']);
+    return gulp.src([
+        paths.src + '/**/*.js',
+        'gulpfile.js',
+        'package.json'
+        ])
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'));
 });
 
 
-gulp.task('clean', function() {
-	return gulp.src(paths.build + '**/*.*', { read: false })
-		.pipe(plugins.clean());
+// Serves locals files in browsers
+gulp.task('serve', function() {
+    browserSync({
+        server: { baseDir: paths.dist }
+    });
+
+    gulp.watch(paths.src + '/**/*.scss', ['build:css']);
+    gulp.watch(paths.src + '/**/*.js', ['build:js', browserSync.reload]);
+    gulp.watch(paths.src + '/**/*.html', ['build:html', browserSync.reload]);
 });
 
+
+// Deletes the dist directory
+gulp.task('clean', function(done) {
+    del([paths.dist], done);
+});
+
+
+// gulp bump --type major|minor|patch
+// - Bumps up package.json version
 gulp.task('bump', function() {
-	// If --major or --minor isn't set, bump version defaults to patch
-	var type = env.major ? 'major' : (env.minor ? 'minor' : 'patch');
-
-	return gulp.src('./package.json')
-		.pipe(plugins.bump({ type: type }))
-		.pipe(gulp.dest('./'));
+    var type = $.util.env.type || 'patch';
+    return gulp.src('./package.json')
+        .pipe($.bump({ type: type }))
+        .pipe(gulp.dest('./'));
 });
 
 // TODO: Add testing tasks
